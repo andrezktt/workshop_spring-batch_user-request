df4768e42f27396295f0cd3fdd4c6cd1f7b8bc08
andre_zktt <andrezicatti@gmail.com>
2025-03-25 10:54:19 -0300

    :tada: init: Create Project

---
+++ src/main/java/com/andrezktt/workshop_spring_batch_user_request/WorkshopSpringBatchUserRequestApplication.java
@@ -0,0 +1,13 @@
+package com.andrezktt.workshop_spring_batch_user_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBatchUserRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBatchUserRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrezktt/workshop_spring_batch_user_request/WorkshopSpringBatchUserRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrezktt.workshop_spring_batch_user_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBatchUserRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
